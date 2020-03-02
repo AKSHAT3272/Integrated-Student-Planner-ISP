02598b2b085537b2727b7bad22d91b5780914f3f
Akshat Patel <akshatpatel@mail.adelphi.edu>
2020-03-02 18:22:02 -0500

    S20SS2GB #comment Added token based password reset. We still need to add email confirmation, currently the system works would confirming emails for the registered users. #time 4h 30m

--- project/__init__.py
+++ project/__init__.py
@@ -2,37 +2,49 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager 
+from flask_login import LoginManager
+from flask_mail import Mail, Message
+from flask_httpauth import HTTPBasicAuth
+
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+auth = HTTPBasicAuth()
+auth_token = HTTPBasicAuth()
+app = Flask(__name__)
+app.config.update(
+	#DEBUG=True,
+	#EMAIL SETTINGS
+	MAIL_SERVER='localhost',
+	MAIL_PORT=587,
+	MAIL_USE_TLS=True,
+    	)
+mail = Mail(app)
+#def create_app():
+    #app = Flask(__name__)
+mail.init_app(app)
+app.config['SECRET_KEY'] = 'p4K5pYXDm4McH53R5mB9tCYpy'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def create_app():
-    app = Flask(__name__)
+db.init_app(app)
 
-    app.config['SECRET_KEY'] = 'p4K5pYXDm4McH53R5mB9tCYpy'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    db.init_app(app)
+from .models import User
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    from .models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
+@login_manager.user_loader
+def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+#return app
--- project/auth.py
+++ project/auth.py
@@ -3,10 +3,34 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
+from itsdangerous import URLSafeTimedSerializer
+from threading import Thread
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email
+from flask_mail import Message
 from .models import User
-from . import db
+from . import db, mail
+from project import app
+import os
+
+
+
+
+ts = URLSafeTimedSerializer(os.urandom(24))
+
 
 auth = Blueprint('auth', __name__)
+
+def send_async_email(msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(recipients,subject, html_body):
+    msg = Message(subject,sender = 'akshat@isp.profleune.net', recipients=[recipients])
+    msg.html = html_body
+    thr = Thread(target=send_async_email, args=[msg])
+    thr.start()
 
 @auth.route('/login')
 def login():
@@ -22,7 +46,7 @@ def login_post():
 
     # check if user actually exists
     # take the user supplied password, hash it, and compare it to the hashed password in database
-    if not user or not check_password_hash(user.password, password): 
+    if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
 
@@ -43,7 +67,7 @@ def signup_post():
 
     user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
 
-    if user: # if a user is found, we want to redirect back to signup page so user can try again  
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
 
@@ -56,8 +80,55 @@ def signup_post():
 
     return redirect(url_for('auth.login'))
 
+@auth.route('/reset')
+def reset():
+    return render_template('reset.html')
+
+@auth.route('/reset', methods=['GET','POST'])
+def reset_post():
+
+    email = request.form.get('email')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        subject = "Password reset requested"
+        token = ts.dumps(user.email, salt='recover-key')
+        recover_url = url_for('auth.reset_with_token', token=token, _external=True)
+        html=render_template('recover.html', recover_url=recover_url)
+        send_email(user.email, subject, html)
+        return redirect(url_for('auth.login'))
+
+    #return render_template('reset.html')
+
+    else:
+        flash('Email does not exists')
+        return redirect(url_for('auth.login'))
+
+@auth.route('/reset/<token>', methods=['GET','POST'])
+def reset_with_token(token):
+    try:
+        email= ts.loads(token, salt="recover-key", max_age = 86400)
+    except:
+        abort(404)
+
+    form = PasswordForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=email).first()
+        password = request.form.get('password')
+        user.password = generate_password_hash(password, method='pbkdf2:sha256')
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+
+    return render_template('reset_with_token.html', form=form, token=token)
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+class PasswordForm(Form):
+    password = PasswordField('Password', validators=[DataRequired()])
